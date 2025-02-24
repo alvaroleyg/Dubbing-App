202e3988e7f78c421ab659434bd6da133e54e8c9
Álvaro Ley <alvaroleyg@gmail.com>
2025-02-24 08:53:35 +0100

    eliminacion carpeta 'OLD'

--- OLD/kimi.ai/server.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $audioData = file_get_contents('php://input');
-    $filePath = 'recordings/recording_' . time() . '.wav';
-    file_put_contents($filePath, $audioData);
-    echo $filePath;
-}
-?>
